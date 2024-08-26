fb1ce417c16fab73aa13e1cc1d7ec0ffdfc6ad90
I am Samir <itzsamirgaming@gmail.com>
2024-08-26 20:20:39 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 function toggleMenu() {
-    const sidebar = document.getElementById("sidebar");
+    var sidebar = document.getElementById("sidebar");
     if (sidebar.style.width === "250px") {
         sidebar.style.width = "0";
+        document.getElementById("main-content").style.marginLeft = "0";
     } else {
         sidebar.style.width = "250px";
+        document.getElementById("main-content").style.marginLeft = "250px";
     }
 }
