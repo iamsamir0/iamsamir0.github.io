8536d45aef41910c9d6ec19f85ff00d8304da693
I am Samir <itzsamirgaming@gmail.com>
2024-10-01 20:01:35 +0600

    Delete js/script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var sidebar = document.getElementById("sidebar");
-    if (sidebar.style.width === "250px") {
-        sidebar.style.width = "0";
-    } else {
-        sidebar.style.width = "250px";
-    }
-}
