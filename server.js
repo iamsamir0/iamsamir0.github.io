dc4981bb79b5ad6ab12b50b444fbcb5bbf19bd7f
I am Samir <itzsamirgaming@gmail.com>
2024-10-20 20:48:06 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/database');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
@@ -11,8 +12,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'views')));
+
 app.use('/api/auth', authRoutes);
 
+// Start the server
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
